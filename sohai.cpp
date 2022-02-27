36b6a4cf57879c72651c46d1dcbd85f7366e262b
PM <perminggwee@gmail.com>
2022-02-27 17:16:10 +0800

    sohai

--- sohai.cpp
+++ sohai.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // main() is where program execution begins.
 int main() {
    cout << "Call me sohai gih" <<endl; // prints Hello World
-   cout << "Call me sohai mingx2" <<endl; // prints Hello World
+   cout << "Call me sohai " <<endl; // prints Hello World
 
    return 0;
 }
