ada31287c9752e3ffcbc53786b492a3f9b84d47f
PM <perminggwee@gmail.com>
2022-02-27 17:12:06 +0800

    ming

---
+++ ming.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+   cout << "Call me sohai gih" <<endl; // prints Hello World
+   cout << "Call me sohai ming" <<endl; // prints Hello World
+
+   return 0;
+}
